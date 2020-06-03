cdb6976d8140f22bd99102a9b9150264b1728e7d
Andrew Kim <andrewswkim47@gmail.com>
2020-06-02 20:49:58 -0700

    create game manager methods and start to create a new rapper object

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -1,5 +1,18 @@
 let GameManager = {
-  name: function() {
-    player = new Player();
+  setGameStart: function(rapStyle) {
+    this.resetRapper(rapStyle);
+    this.setPreBattle();
+  },
+  resetRapper: function(rapStyle) {
+    switch (rapStyle) {
+      case "Marlon":
+        rapper = new Rapper(rapStyle, 100, 50, 25, 50, 75);
+        break;
+      default:
+
+    }
+  },
+  setPreBattle: function() {
+
   }
 }
