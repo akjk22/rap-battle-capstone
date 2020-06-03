80f02d1f5f190ca7664c8e183eaa12289396bcc0
Andrew Kim <andrewswkim47@gmail.com>
2020-06-02 21:26:22 -0700

    attempt to add avatar

--- js/gamemanager.js
+++ js/gamemanager.js
@@ -5,12 +5,19 @@ let GameManager = {
   },
   resetRapper: function(rapStyle) {
     switch (rapStyle) {
-      case "Marlon":
+      case "Marlon Craft":
         rapper = new Rapper(rapStyle, 100, 50, 25, 50, 75);
         break;
-      default:
-
+      case "J. Cole":
+        rapper = new Rapper(rapStyle, 200, 100, 75, 100, 75);
+        break;
+      case "Andre 3000":
+        rapper = new Rapper(rapStyle, 150, 100, 50, 100, 100);
+        break;
     }
+    //grab interface so we can change it upon rapper selection
+    let getInterface = document.querySelector(".interface");
+    getInterface.innerHTML = '<img src="">';
   },
   setPreBattle: function() {
 
