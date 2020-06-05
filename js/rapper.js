0732ca8a97c2a1d4d70be5dbd78df01db7517add
Andrew Kim <andrewswkim47@gmail.com>
2020-06-04 21:02:01 -0700

    start working on calculating damage

--- js/rapper.js
+++ js/rapper.js
@@ -10,3 +10,22 @@ function Rapper(rapStyle, hp, special, strength, wordplay, delivery) {
   this.delivery = delivery;
 }
 
+let RapperMoves = {
+  calcAttack: function() {
+    // who will attack first?
+    let getRapperSpecial = rapper.special;
+    let getFoeSpecial = foe.special;
+    // rapper attacks/spits bars
+    let rapperAttack = function() {
+    let calcBaseDamage;
+    if (rapper.wordplay > 0 ) {
+      calcBaseDamage = rapper.strength * rapper.wordplay / 1000;
+    } else {
+      calcBaseDamage = rapper.strength * rapper.delivery / 1000;
+    }
+    let offsetDamage = Math.floor(Math.random() * Math.floor(10));
+    let calcOutputDamage = calcBaseDamage + offsetDamage; 
+  }
+  }
+ 
+}
