642a37af9598243a504db496a7ae5f79f930840c
Andrew Kim <andrewswkim47@gmail.com>
2020-06-11 21:05:59 -0700

    having issues with rapid api

--- js/example.js
+++ js/example.js
@@ -1,7 +1,23 @@
-function fetchData() {
-  fetch("https://api.genius.com/songs/378195").then(response => {
-    console.log(response);
-  });
-}
+// function fetchData() {
+//   fetch("https://mourits-lyrics.p.rapidapi.com/").then(response => {
+//     return response.json();
+//   }).then(data => {
+//     console.log(data);
+//   });
+// }
 
-fetchData()
+// fetchData()
+
+fetch("https://sridurgayadav-chart-lyrics-v1.p.rapidapi.com/apiv1.asmx/SearchLyricDirect?artist=michael%2520jackson&song=bad", {
+	"method": "GET",
+	"headers": {
+		"x-rapidapi-host": "sridurgayadav-chart-lyrics-v1.p.rapidapi.com",
+		"x-rapidapi-key": "324b68028emsh2bdaa9e9f17a4efp16daedjsn355e4f3cbef3"
+	}
+})
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+});
