bf717802150d94342b91a10620ef251398a52d66
Andrew Kim <andrewswkim47@gmail.com>
2020-06-14 11:33:35 -0700

    still playing with fetch api. Able to see song titles in content array in console.

--- js/example.js
+++ js/example.js
@@ -1,3 +1,5 @@
+//one
+
 // function fetchData() {
 //   fetch("https://mourits-lyrics.p.rapidapi.com/").then(response => {
 //     return response.json();
@@ -8,16 +10,37 @@
 
 // fetchData()
 
-fetch("https://canarado-lyrics.p.rapidapi.com/lyrics/zenith%2520denzel%2520curry", {
-	"method": "GET",
-	"headers": {
-		"x-rapidapi-host": "canarado-lyrics.p.rapidapi.com",
-		"x-rapidapi-key": "324b68028emsh2bdaa9e9f17a4efp16daedjsn355e4f3cbef3"
-	}
-})
-.then(response => {
-	console.log(response);
-})
-.catch(err => {
-	console.log(err);
-});
+// two
+
+function fetchData() {
+  fetch("https://canarado-lyrics.p.rapidapi.com/lyrics/zenith%2520denzel%2520curry", {
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "canarado-lyrics.p.rapidapi.com",
+      "x-rapidapi-key": "324b68028emsh2bdaa9e9f17a4efp16daedjsn355e4f3cbef3"
+    }
+  }).then(response => {
+    return response.json();
+  }).then(data => {
+    console.log(data);
+  });
+}
+
+fetchData()
+
+// three
+
+// fetch("https://canarado-lyrics.p.rapidapi.com/lyrics/zenith%2520denzel%2520curry", {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "canarado-lyrics.p.rapidapi.com",
+// 		"x-rapidapi-key": "324b68028emsh2bdaa9e9f17a4efp16daedjsn355e4f3cbef3"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.log(err);
+// });
+
